5b2335a4df24b045733123fce0ff643d105ce1b6
ViaApp Team <117312902+ViaAppTeam@users.noreply.github.com>
2024-06-16 14:48:35 +0300

    Update interactionConfig.js

--- src/Config/interactionConfig.js
+++ src/Config/interactionConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
     embedColor: "#2b2d31",
-    embedImage: "https://cdn.discordapp.com/attachments/1134203995079053493/1140337912551059477/20230808_150141_0000.png",
-    embedThumbnail: "https://cdn.discordapp.com/attachments/1134203995079053493/1140337912769159178/IMG_20230808_010334_984.jpg"
+    embedImage: "embedImage",
+    embedThumbnail: "embedThumbnail"
 }
